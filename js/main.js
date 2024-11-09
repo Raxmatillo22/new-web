81943e92ff725cedc76cf3b4f1e06556a638f5be
Raxmatillo <rustamovraxmatillo07@gmail.com>
2024-11-09 17:02:08 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Swiper
+var swiper = new swiper(".home", {
+  spaceBetween: 3,
+  canteredSlides: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
